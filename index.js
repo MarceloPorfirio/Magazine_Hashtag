a34dc72d5853938fc793ddc08dee3475904c370b
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2023-08-31 12:33:46 -0300

    add

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert('Ola Mundo!')
+const produto1 = {
+    nome: 'Casaco Branco',
+    marca: 'Zara',
+    preco: 70,
+    nomeArquivoImagem: 'product-1.jpg'
+}
+
+const cartaoProduto = `<div id="card-produto-1">
+<img
+    src="./assets/img/${produto1.nomeArquivoImagem}"
+    alt="Produto 1 do Magazine Hashtag."
+    style="height: 300px"
+    />
+<p>${produto1.marca}</p>
+<p>${produto1.nome}</p>
+<p>${produto1.preco}</p>
+<button>Adicionar</Button>
+</div>
+
+`
+document.getElementById('container-produto').innerHTML +=cartaoProduto
