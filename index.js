0ebfcca3db5ecd4f05a074b20042dec53026a569
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2023-09-01 12:39:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-const produto1 = {
-    nome: 'Casaco Branco',
-    marca: 'Zara',
-    preco: 70,
-    nomeArquivoImagem: 'product-1.jpg'
-}
+const catalogo = [{
+        id: 1,
+        nome: 'Casaco Branco',
+        marca: 'Zara',
+        preco: 70,
+        nomeArquivoImagem: 'product-1.jpg'
+},{
+        id: 2,
+        nome: 'Casaco Branco',
+        marca: 'Zara',
+        preco: 70,
+        nomeArquivoImagem: 'product-1.jpg'
+},{
+        id: 3,
+        nome: 'Casaco Branco',
+        marca: 'Zara',
+        preco: 70,
+        nomeArquivoImagem: 'product-1.jpg'
+},{
+        id: 4,
+        nome: 'Casaco Branco',
+        marca: 'Zara',
+        preco: 70,
+        nomeArquivoImagem: 'product-1.jpg'
+},{
+        id: 5,
+        nome: 'Casaco Branco',
+        marca: 'Zara',
+        preco: 70,
+        nomeArquivoImagem: 'product-1.jpg'
+}]
 
-const cartaoProduto = `<div id="card-produto-1">
+for( const produtoCatalogo of catalogo){
+    const cartaoProduto = `<div id="card-produto-1">
 <img
-    src="./assets/img/${produto1.nomeArquivoImagem}"
+    src="./assets/img/${produtoCatalogo.nomeArquivoImagem}"
     alt="Produto 1 do Magazine Hashtag."
     style="height: 300px"
     />
-<p>${produto1.marca}</p>
-<p>${produto1.nome}</p>
-<p>${produto1.preco}</p>
+<p>${produtoCatalogo.marca}</p>
+<p>${produtoCatalogo.nome}</p>
+<p>${produtoCatalogo.preco}</p>
 <button>Adicionar</Button>
 </div>
 
 `
 document.getElementById('container-produto').innerHTML +=cartaoProduto
+}
